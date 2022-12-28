d9bbec125756541662c93ddfada9f79383ae9e73
Tristan F <LeoDog896@hotmail.com>
2022-12-28 12:38:14 -0500

    Make website

---
+++ www/index.ts
@@ -0,0 +1,20 @@
+import { ModelViewerElement } from "@google/model-viewer"
+import * as download from "downloadjs"
+
+const maxwellURL = new URL("../object/maxwell.glb", import.meta.url).href;
+
+const button = document.querySelector("button#download")!;
+
+button.addEventListener("click", () => {
+    // download file from maxwellURL
+    download(maxwellURL);
+});
+
+const viewer = new ModelViewerElement();
+viewer.src = maxwellURL;
+viewer.alt = "Maxwell the Carryable Cat"
+viewer.cameraControls = true;
+viewer.shadowIntensity = 1;
+viewer.style.width = "100%";
+viewer.style.height = "100%";
+document.querySelector("div#maxwell")!.appendChild(viewer);
